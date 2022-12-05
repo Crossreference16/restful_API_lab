516831532cb60ad6e6aeb30092cba5c466f19a9f
Khalil <khalil.hersi@hotmail.co.uk>
2022-12-05 18:46:12 +0000

    adds RequestParams, changing local host to 'greeting?khalil&ToD'

--- src/main/java/com/example/task_01/controllers/Controller.java
+++ src/main/java/com/example/task_01/controllers/Controller.java
@@ -4,15 +4,20 @@ import com.example.task_01.models.Greeting;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
     @GetMapping("/greeting")
-    public String myGreeting (){
+    public String myGreeting (
+            @RequestParam(value = "khalil") String khalilName,
+            @RequestParam(value = "ToD")String morningTimeOfDay){
         Greeting greeting = new Greeting("Khalil", "morning");
         return "Good " + greeting.getTimeOfDay() + ", " + " " + greeting.getName() + "!";
+
+
     }
 
 //    @GetMapping("/greeting")
