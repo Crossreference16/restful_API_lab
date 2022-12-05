32ac286c970e91c0b1b13d1a60de565ca428b040
Khalil <khalil.hersi@hotmail.co.uk>
2022-12-05 15:22:20 +0000

    adds last bits to GetMapping Method. It now uses POJO instead of string.

--- src/main/java/com/example/task_01/controllers/Controller.java
+++ src/main/java/com/example/task_01/controllers/Controller.java
@@ -1,5 +1,8 @@
 package com.example.task_01.controllers;
 
+import com.example.task_01.models.Greeting;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/greeting")
-    public String greeting (){
-        return "Good morning!";
+    public String myGreeting (){
+        Greeting greeting = new Greeting("Khalil", "morning");
+        return "Good " + greeting.getTimeOfDay() + ", " + " " + greeting.getName() + "!";
     }
 
+//    @GetMapping("/greeting")
+//    public ResponseEntity<Greeting>makeGreeting(){
+//        Greeting greeting = new Greeting("Khalil", "Morning");
+//        return new ResponseEntity<>(greeting, HttpStatus.OK);
+//    }
 
 
 
